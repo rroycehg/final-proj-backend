9aa77a30274e9e46a62a981902ec3344eca16655
roli <rroycehg@gmail.com>
2021-09-09 19:51:09 -0400

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   resources :battles, only: [:index, :show, :create]
   resources :partyslots, only: [:index, :show, :update, :create, :destroy]
-  resources :users, only: [:index, :show. :create]
+  resources :users, only: [:index, :show, :create]
   resources :characters, only: [:index, :show]
   resources :classtypes, only: [:index, :show]
   resources :roles, only: [:index, :show]
